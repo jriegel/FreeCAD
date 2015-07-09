c9ce94c684f7850860bd31eeb5fd18913bc422d7
DeepSOIC <vv.titov@gmail.com>
2015-07-09 22:04:13 +0300

    Sketcher: when refusing to link external, print why (in statusbar)

--- src/Mod/Sketcher/App/SketchObject.cpp
+++ src/Mod/Sketcher/App/SketchObject.cpp
@@ -1632,13 +1632,26 @@ int SketchObject::trim(int GeoId, const Base::Vector3d& point)
     return -1;
 }
 
-bool SketchObject::isExternalAllowed(App::Document *pDoc, App::DocumentObject *pObj) const
+bool SketchObject::isExternalAllowed(App::Document *pDoc, App::DocumentObject *pObj, eReasonList* rsn) const
 {
+    if (rsn)
+        *rsn = rlAllowed;
+
     // Externals outside of the Document are NOT allowed
-    if (this->getDocument() != pDoc)
+    if (this->getDocument() != pDoc){
+        if (rsn)
+            *rsn = rlOtherDoc;
         return false;    
+    }
+
+    //circular reference prevention
     try {
-        return this->testIfLinkDAGCompatible(pObj);
+        if (!(this->testIfLinkDAGCompatible(pObj))){
+            if (rsn)
+                *rsn = rlCircularReference;
+            return false
+            ;
+        }
     } catch (Base::Exception &e) {
         Base::Console().Warning("Probably, there is a circular reference in the document. Error: %s\n", e.what());
         return true; //prohibiting this reference won't remove the problem anyway...
@@ -1651,8 +1664,11 @@ bool SketchObject::isExternalAllowed(App::Document *pDoc, App::DocumentObject *p
     if (body != NULL) {
         if (Part::BodyBase::findBodyOf(pObj) != body) {
             // Selection outside of body not allowed if flag is not set
-            if (!this->allowOtherBody)
+            if (!this->allowOtherBody){
+                if (rsn)
+                    *rsn = rlOtherPart;
                 return false;
+            }
         }
 
         // Datum features are always allowed
